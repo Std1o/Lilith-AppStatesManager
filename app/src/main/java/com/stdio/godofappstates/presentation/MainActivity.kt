73028a5f8dc926d3cbdfc6f68cec39b7d4274941
Std1o <37378410+Std1o@users.noreply.github.com>
2024-11-24 15:32:25 +0300

    lastOperationState checking

--- app/src/main/java/com/stdio/godofappstates/presentation/MainActivity.kt
+++ app/src/main/java/com/stdio/godofappstates/presentation/MainActivity.kt
@@ -1,15 +1,46 @@
 package com.stdio.godofappstates.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.stdio.godofappstates.R
+import com.stdio.godofappstates.domain.operationState.OperationState
+import com.stdio.godofappstates.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import stdio.godofappstates.annotations.AllStatesReadyToUse
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.lastOperationState.collect { uiState ->
+                    progressBar.isVisible = uiState is OperationState.Loading
+                    when (uiState) {
+                        is OperationState.Empty204 -> {}
+                        is OperationState.Error -> textView.text = "Error"
+                        is OperationState.Loading -> textView.text = "Loading..."
+                        is OperationState.NoState -> {}
+                        is OperationState.Success -> textView.text = "Success"
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/stdio/godofappstates/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/stdio/godofappstates/presentation/viewmodel/MainViewModel.kt
@@ -1,9 +1,31 @@
 package com.stdio.godofappstates.presentation.viewmodel;
 
+import androidx.lifecycle.viewModelScope
+import com.stdio.godofappstates.domain.operationState.OperationState
+import com.stdio.godofappstates.domain.operationState.protect
 import dagger.hilt.android.lifecycle.HiltViewModel
 import godofappstates.presentation.viewmodel.StatesViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : StatesViewModel() {
+
+    init {
+        makeRequest()
+    }
+
+    private fun makeRequest() {
+        viewModelScope.launch {
+            executeOperation(
+                { fakeRequest() }, String.Companion::class.java::class
+            ).protect()
+        }
+    }
+
+    suspend fun fakeRequest(): OperationState<String> {
+        delay(7000)
+        return OperationState.Success("Hello")
+    }
 }
